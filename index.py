71ce9a7c110cef42070672575fd3bce92d98d788
Nevindu12 <nilangithnevindu@gmail.com>
2023-07-11 14:42:40 +0530

    full project

--- index.py
+++ index.py
@@ -12,7 +12,7 @@ def ConstBoard(board):
     print("\n\n");
 
 def User1Turn(board):
-    pos = input("Enter X's position from [1,2,3,..., 9]");
+    pos = input("Enter X's position from [1,2,3,..., 9]: ");
     pos = int(pos);
     if(board[pos-1]!=0):
         print("Wrong Move");
@@ -20,17 +20,55 @@ def User1Turn(board):
     board[pos-1]=-1;
 
 def User2Turn(board):
-    pos = input("Enter O's position from [1,2,3,..., 9]");
+    pos = input("Enter O's position from [1,2,3,..., 9]: ");
     pos = int(pos);
     if(board[pos-1]!=0):
         print("Wrong Move");
         exit(0);
     board[pos-1]=1;
 
+def minmax(board, player):
+    x = analayzeboard(board);
+    if(x!=0):
+        return(x*player);    
+    pos = -1;
+    value = -2;
+    for i in range(0, 9):
+        if(board[i]==0):
+            board[i]=player;
+            score = -minmax(board, player*-1);
+            board[i] = 0;
+            if(score>value):
+                value = score;
+                pos = i;
+    if(pos==-1):
+        return 0;
+    return value;
 
+def CompTurn(board):
+    pos = -1;
+    value = -2;
+    for i in range(0, 9):
+        if(board[i]==0):
+            board[i]=1;
+            score = -minmax(board, -1);
+            board[i] = 0;
+            if(score>value):
+                value = score;
+                pos = i;
+    board[pos]=1
+
+
+def analayzeboard(board):
+    cb = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
+    for i in range(0, 8):
+        if(board[cb[i][0]]!=0 and board[cb[i][0]]==board [cb[i][1]] and board[cb[i][0]]==board [cb[i][2]]):
+            return board[cb[i][0]];
+    return 0;
 
 def main():
-    choice = input("Enter 1 for Single Player, Enter 2 for Multiplayer:");
+    choice = input("Enter 1 for Single Player, Enter 2 for Multiplayer: ");
     choice = int(choice);
     board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     if(choice==1):
@@ -66,3 +104,6 @@ def main():
     if(x==1):
         ConstBoard(board);
         print("Player O WINS!!!! & X Looses!");
+
+if __name__ == '__main__':
+    main()
